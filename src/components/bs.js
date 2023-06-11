85bb4607c31350348a0d7538409814d2eb3174b7
Vikash Sangai <vikash36905@gmail.com>
2023-06-11 17:36:31 +0530

    added question list

--- src/components/bs.js
+++ src/components/bs.js
@@ -1,14 +1,14 @@
 export const bstasks = {
     task : [
     {
-        name: "Koko eating Bananas",
-        link: "https://leetcode.com/problems/koko-eating-bananas/",
+        name: "Search Insert Position",
+        link: "https://leetcode.com/problems/search-insert-position/",
         id: 1,
         checked: false
     },
     {
-        name: "Aggressive Cows",
-        link: "https://www.spoj.com/problems/AGGRCOW/",
+        name: "Koko eating Bananas",
+        link: "https://leetcode.com/problems/koko-eating-bananas/",
         id: 2,
         checked : false
     },
@@ -16,6 +16,48 @@ export const bstasks = {
         name: "Sqrt(x)",
         link: "https://leetcode.com/problems/sqrtx/",
         id: 3,
+        checked : false
+    },
+    {
+        name: "Peak Index in a Mountain Array",
+        link: "https://leetcode.com/problems/peak-index-in-a-mountain-array/",
+        id: 4,
+        checked: false
+    },
+    {
+        name: "Search in Rotated Sorted Array",
+        link: "https://leetcode.com/problems/search-in-rotated-sorted-array/",
+        id: 5,
+        checked: false
+    },
+    {
+        name: "Minimize the Maximum of Two Arrays",
+        link: "https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/",
+        id: 6,
+        checked: false
+    },
+    {
+        name: "Aggressive Cows",
+        link: "https://www.spoj.com/problems/AGGRCOW/",
+        id: 7,
+        checked: false
+    },
+    {
+        name: "Arranging Coins",
+        link: "https://leetcode.com/problems/arranging-coins/",
+        id: 8,
+        checked: false
+    },
+    {
+        name: "Maximum Number of Robots Within Budget",
+        link: "https://leetcode.com/problems/maximum-number-of-robots-within-budget/",
+        id: 9,
+        checked: false
+    },
+    {
+        name: "Minimum Time to Repair Cars",
+        link: "https://leetcode.com/problems/minimum-time-to-repair-cars/",
+        id: 10,
         checked: false
     }
 ]
--- src/components/dp.js
+++ src/components/dp.js
@@ -11,6 +11,90 @@ export const dptasks = {
         link: "https://atcoder.jp/contests/dp/tasks/dp_a",
         id: 2,
         checked : false
+    },
+    {
+        name: "Trapping Rain Water",
+        link: "https://leetcode.com/problems/trapping-rain-water/",
+        id: 3,
+        checked : false
+    },
+    {
+        name: "Wildcard Matching",
+        link: "https://leetcode.com/problems/wildcard-matching/",
+        id: 4,
+        checked : false
+    },
+    {
+        name: "Unique Paths",
+        link: "https://leetcode.com/problems/unique-paths/",
+        id: 5,
+        checked : false
+    },
+    {
+        name: "Edit Distance",
+        link: "https://leetcode.com/problems/edit-distance/",
+        id: 6,
+        checked : false
+    },
+    {
+        name: "Triangle",
+        link: "https://leetcode.com/problems/triangle/",
+        id: 7,
+        checked : false
+    },
+    {
+        name: "Best Time to Buy and Sell Stock III",
+        link: "https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/",
+        id: 8,
+        checked : false
+    },
+    {
+        name: "Word Break",
+        link: "https://leetcode.com/problems/word-break/",
+        id: 9,
+        checked : false
+    },
+    {
+        name: "Perfect Squares",
+        link: "https://leetcode.com/problems/perfect-squares/",
+        id: 10,
+        checked : false
+    },
+    {
+        name: "Longest Increasing Subsequence",
+        link: "https://leetcode.com/problems/longest-increasing-subsequence/",
+        id: 11,
+        checked : false
+    },
+    {
+        name: "Coin Change",
+        link: "https://leetcode.com/problems/coin-change/",
+        id: 12,
+        checked : false
+    },
+    {
+        name: "Delete Operation for Two Strings",
+        link: "https://leetcode.com/problems/delete-operation-for-two-strings/",
+        id: 13,
+        checked : false
+    },
+    {
+        name: "Uncrossed Lines",
+        link: "https://leetcode.com/problems/uncrossed-lines/",
+        id: 14,
+        checked : false
+    },
+    {
+        name: "Best Team With No Conflicts",
+        link: "https://leetcode.com/problems/best-team-with-no-conflicts/",
+        id: 15,
+        checked : false
+    },
+    {
+        name: "Restore The Array",
+        link: "https://leetcode.com/problems/restore-the-array/",
+        id: 16,
+        checked : false
     }
 ]
 };
