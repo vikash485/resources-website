f4b9d99c71cb280ee580a4cc5c0a9ecf3adcedb7
Vikash Sangai <vikash36905@gmail.com>
2023-07-09 19:43:51 +0530

    changed navbar heading

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const NavBar =() => {
       <div id='main-nav'>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Selected Questions for DSA</Link>
+                <Link className="navbar-brand" to="/">AlgoHub</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
